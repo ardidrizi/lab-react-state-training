f68b1f9ad6d59d60ce04c9dca47edb6a48901a8a
Ardian Idrizi <ardianidizi@gmail.com>
2024-10-10 17:59:50 +0200

    Dice update random roll

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
 export const Dice = () => {
-  const randomDiceNumber = Math.floor(Math.random() * 6) + 1;
-  const [dice, setDice] = useState(randomDiceNumber);
+  const [dice, setDice] = useState(Math.floor(Math.random() * 6) + 1);
 
-  const randomDice = () => {
+  const rollDice = () => {
+    const randomDiceNumber = Math.floor(Math.random() * 6) + 1;
     setDice(0);
     setTimeout(() => {
       setDice(randomDiceNumber);
@@ -12,7 +12,7 @@ export const Dice = () => {
   };
 
   return (
-    <div onClick={randomDice}>
+    <div onClick={rollDice}>
       <img
         className="dice"
         src={`/src/assets/images/dice${dice}.png`}
